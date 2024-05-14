b60fe7ff66ecc3d5db8b8f5d72134ff1c54c1dff
muhu-18 <94680131+muhu-18@users.noreply.github.com>
2024-05-14 23:50:11 +0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // Change this to your database server name
 $username = "username"; // Change this to your database username
 $password = ""; // Change this to your database password
-$dbname = "Contact"; // Change this to your database name
+$dbname = "contact"; // Change this to your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
